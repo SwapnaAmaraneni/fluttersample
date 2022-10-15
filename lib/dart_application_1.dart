c5cad116accbbcb7665c7dc1645d3464f6d30b3e
SwapnaAmaraneni <114658781+SwapnaAmaraneni@users.noreply.github.com>
2022-10-15 12:59:23 +0530

    initial commit

---
+++ lib/dart_application_1.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("hello");
+
+  int a = 10;
+  int b = 20;
+  int z = a + b;
+  print(z);
+
+  String name = 'swapna';
+  print(name);
+  print("upper case is ${name.toUpperCase()}");
+  
+}
